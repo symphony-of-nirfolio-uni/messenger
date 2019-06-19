59eb687d90fd3e4af07a68ee053fc5d9482e8349
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2019-06-19 18:29:43 +0300

    Added message textbox

--- Messenger/Messenger/Messenger.Designer.cs
+++ Messenger/Messenger/Messenger.Designer.cs
@@ -30,89 +30,153 @@ namespace Messenger
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
-			this.controlSplitContainer = new System.Windows.Forms.SplitContainer();
-			this.listOfChatFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
-			this.chatSplitContainer = new System.Windows.Forms.SplitContainer();
-			((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
-			this.mainSplitContainer.Panel1.SuspendLayout();
-			this.mainSplitContainer.Panel2.SuspendLayout();
-			this.mainSplitContainer.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.controlSplitContainer)).BeginInit();
-			this.controlSplitContainer.Panel2.SuspendLayout();
-			this.controlSplitContainer.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.chatSplitContainer)).BeginInit();
-			this.chatSplitContainer.SuspendLayout();
+			this.main_SplitContainer = new System.Windows.Forms.SplitContainer();
+			this.control_SplitContainer = new System.Windows.Forms.SplitContainer();
+			this.listOfChat_FlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+			this.chat_SplitContainer = new System.Windows.Forms.SplitContainer();
+			this.messages_SplitContainer = new System.Windows.Forms.SplitContainer();
+			this.chat_FlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+			this.message_TextBox = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.main_SplitContainer)).BeginInit();
+			this.main_SplitContainer.Panel1.SuspendLayout();
+			this.main_SplitContainer.Panel2.SuspendLayout();
+			this.main_SplitContainer.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.control_SplitContainer)).BeginInit();
+			this.control_SplitContainer.Panel2.SuspendLayout();
+			this.control_SplitContainer.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chat_SplitContainer)).BeginInit();
+			this.chat_SplitContainer.Panel2.SuspendLayout();
+			this.chat_SplitContainer.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.messages_SplitContainer)).BeginInit();
+			this.messages_SplitContainer.Panel1.SuspendLayout();
+			this.messages_SplitContainer.Panel2.SuspendLayout();
+			this.messages_SplitContainer.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// mainSplitContainer
+			// main_SplitContainer
 			// 
-			this.mainSplitContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-			this.mainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.mainSplitContainer.Location = new System.Drawing.Point(0, 0);
-			this.mainSplitContainer.Name = "mainSplitContainer";
+			this.main_SplitContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.main_SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.main_SplitContainer.IsSplitterFixed = true;
+			this.main_SplitContainer.Location = new System.Drawing.Point(0, 0);
+			this.main_SplitContainer.Name = "main_SplitContainer";
 			// 
-			// mainSplitContainer.Panel1
+			// main_SplitContainer.Panel1
 			// 
-			this.mainSplitContainer.Panel1.AutoScroll = true;
-			this.mainSplitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-			this.mainSplitContainer.Panel1.Controls.Add(this.controlSplitContainer);
-			this.mainSplitContainer.Panel1MinSize = 180;
+			this.main_SplitContainer.Panel1.AutoScroll = true;
+			this.main_SplitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.main_SplitContainer.Panel1.Controls.Add(this.control_SplitContainer);
+			this.main_SplitContainer.Panel1MinSize = 180;
 			// 
-			// mainSplitContainer.Panel2
+			// main_SplitContainer.Panel2
 			// 
-			this.mainSplitContainer.Panel2.AutoScroll = true;
-			this.mainSplitContainer.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
-			this.mainSplitContainer.Panel2.Controls.Add(this.chatSplitContainer);
-			this.mainSplitContainer.Panel2MinSize = 300;
-			this.mainSplitContainer.Size = new System.Drawing.Size(800, 500);
-			this.mainSplitContainer.SplitterDistance = 266;
-			this.mainSplitContainer.TabIndex = 0;
+			this.main_SplitContainer.Panel2.AutoScroll = true;
+			this.main_SplitContainer.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.main_SplitContainer.Panel2.Controls.Add(this.chat_SplitContainer);
+			this.main_SplitContainer.Panel2MinSize = 300;
+			this.main_SplitContainer.Size = new System.Drawing.Size(800, 500);
+			this.main_SplitContainer.SplitterDistance = 266;
+			this.main_SplitContainer.TabIndex = 0;
 			// 
-			// controlSplitContainer
+			// control_SplitContainer
 			// 
-			this.controlSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.controlSplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-			this.controlSplitContainer.IsSplitterFixed = true;
-			this.controlSplitContainer.Location = new System.Drawing.Point(0, 0);
-			this.controlSplitContainer.Name = "controlSplitContainer";
-			this.controlSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.control_SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.control_SplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.control_SplitContainer.IsSplitterFixed = true;
+			this.control_SplitContainer.Location = new System.Drawing.Point(0, 0);
+			this.control_SplitContainer.Name = "control_SplitContainer";
+			this.control_SplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
 			// 
-			// controlSplitContainer.Panel2
+			// control_SplitContainer.Panel2
 			// 
-			this.controlSplitContainer.Panel2.Controls.Add(this.listOfChatFlowLayoutPanel);
-			this.controlSplitContainer.Size = new System.Drawing.Size(266, 500);
-			this.controlSplitContainer.SplitterDistance = 70;
-			this.controlSplitContainer.TabIndex = 0;
+			this.control_SplitContainer.Panel2.Controls.Add(this.listOfChat_FlowLayoutPanel);
+			this.control_SplitContainer.Size = new System.Drawing.Size(266, 500);
+			this.control_SplitContainer.SplitterDistance = 70;
+			this.control_SplitContainer.TabIndex = 0;
 			// 
-			// listOfChatFlowLayoutPanel
+			// listOfChat_FlowLayoutPanel
 			// 
-			this.listOfChatFlowLayoutPanel.AutoScroll = true;
-			this.listOfChatFlowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.listOfChatFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.listOfChatFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-			this.listOfChatFlowLayoutPanel.Location = new System.Drawing.Point(0, 0);
-			this.listOfChatFlowLayoutPanel.Name = "listOfChatFlowLayoutPanel";
-			this.listOfChatFlowLayoutPanel.Size = new System.Drawing.Size(266, 426);
-			this.listOfChatFlowLayoutPanel.TabIndex = 0;
-			this.listOfChatFlowLayoutPanel.WrapContents = false;
-			this.listOfChatFlowLayoutPanel.SizeChanged += new System.EventHandler(this.ListOfChatFlowLayoutPanel_SizeChanged);
+			this.listOfChat_FlowLayoutPanel.AutoScroll = true;
+			this.listOfChat_FlowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.listOfChat_FlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listOfChat_FlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this.listOfChat_FlowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.listOfChat_FlowLayoutPanel.Name = "listOfChat_FlowLayoutPanel";
+			this.listOfChat_FlowLayoutPanel.Size = new System.Drawing.Size(266, 426);
+			this.listOfChat_FlowLayoutPanel.TabIndex = 0;
+			this.listOfChat_FlowLayoutPanel.WrapContents = false;
+			this.listOfChat_FlowLayoutPanel.SizeChanged += new System.EventHandler(this.ListOfChat_FlowLayoutPanel_SizeChanged);
 			// 
-			// chatSplitContainer
+			// chat_SplitContainer
 			// 
-			this.chatSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.chatSplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-			this.chatSplitContainer.IsSplitterFixed = true;
-			this.chatSplitContainer.Location = new System.Drawing.Point(0, 0);
-			this.chatSplitContainer.Name = "chatSplitContainer";
-			this.chatSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.chat_SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.chat_SplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.chat_SplitContainer.IsSplitterFixed = true;
+			this.chat_SplitContainer.Location = new System.Drawing.Point(0, 0);
+			this.chat_SplitContainer.Name = "chat_SplitContainer";
+			this.chat_SplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
 			// 
-			// chatSplitContainer.Panel1
+			// chat_SplitContainer.Panel1
 			// 
-			this.chatSplitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-			this.chatSplitContainer.Size = new System.Drawing.Size(530, 500);
-			this.chatSplitContainer.SplitterDistance = 70;
-			this.chatSplitContainer.TabIndex = 0;
+			this.chat_SplitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			// 
+			// chat_SplitContainer.Panel2
+			// 
+			this.chat_SplitContainer.Panel2.Controls.Add(this.messages_SplitContainer);
+			this.chat_SplitContainer.Size = new System.Drawing.Size(530, 500);
+			this.chat_SplitContainer.SplitterDistance = 70;
+			this.chat_SplitContainer.TabIndex = 0;
+			// 
+			// messages_SplitContainer
+			// 
+			this.messages_SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.messages_SplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+			this.messages_SplitContainer.IsSplitterFixed = true;
+			this.messages_SplitContainer.Location = new System.Drawing.Point(0, 0);
+			this.messages_SplitContainer.Name = "messages_SplitContainer";
+			this.messages_SplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// messages_SplitContainer.Panel1
+			// 
+			this.messages_SplitContainer.Panel1.Controls.Add(this.chat_FlowLayoutPanel);
+			// 
+			// messages_SplitContainer.Panel2
+			// 
+			this.messages_SplitContainer.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.messages_SplitContainer.Panel2.Controls.Add(this.message_TextBox);
+			this.messages_SplitContainer.Size = new System.Drawing.Size(530, 426);
+			this.messages_SplitContainer.SplitterDistance = 370;
+			this.messages_SplitContainer.TabIndex = 0;
+			// 
+			// chat_FlowLayoutPanel
+			// 
+			this.chat_FlowLayoutPanel.AutoScroll = true;
+			this.chat_FlowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.chat_FlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.chat_FlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+			this.chat_FlowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.chat_FlowLayoutPanel.Name = "chat_FlowLayoutPanel";
+			this.chat_FlowLayoutPanel.Size = new System.Drawing.Size(530, 370);
+			this.chat_FlowLayoutPanel.TabIndex = 0;
+			this.chat_FlowLayoutPanel.WrapContents = false;
+			this.chat_FlowLayoutPanel.SizeChanged += new System.EventHandler(this.Chat_FlowLayoutPanel_SizeChanged);
+			// 
+			// message_TextBox
+			// 
+			this.message_TextBox.AcceptsTab = true;
+			this.message_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.message_TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.message_TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.message_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.message_TextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.message_TextBox.Location = new System.Drawing.Point(48, 15);
+			this.message_TextBox.Multiline = true;
+			this.message_TextBox.Name = "message_TextBox";
+			this.message_TextBox.Size = new System.Drawing.Size(379, 20);
+			this.message_TextBox.TabIndex = 0;
+			this.message_TextBox.WordWrap = false;
+			this.message_TextBox.TextChanged += new System.EventHandler(this.Message_TextBox_TextChanged);
 			// 
 			// Messenger
 			// 
@@ -120,31 +184,40 @@ namespace Messenger
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
 			this.ClientSize = new System.Drawing.Size(800, 500);
-			this.Controls.Add(this.mainSplitContainer);
+			this.Controls.Add(this.main_SplitContainer);
 			this.DoubleBuffered = true;
 			this.MinimumSize = new System.Drawing.Size(500, 500);
 			this.Name = "Messenger";
 			this.Text = "Messenger";
 			this.Load += new System.EventHandler(this.Messenger_Load);
-			this.mainSplitContainer.Panel1.ResumeLayout(false);
-			this.mainSplitContainer.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
-			this.mainSplitContainer.ResumeLayout(false);
-			this.controlSplitContainer.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.controlSplitContainer)).EndInit();
-			this.controlSplitContainer.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.chatSplitContainer)).EndInit();
-			this.chatSplitContainer.ResumeLayout(false);
+			this.main_SplitContainer.Panel1.ResumeLayout(false);
+			this.main_SplitContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.main_SplitContainer)).EndInit();
+			this.main_SplitContainer.ResumeLayout(false);
+			this.control_SplitContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.control_SplitContainer)).EndInit();
+			this.control_SplitContainer.ResumeLayout(false);
+			this.chat_SplitContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chat_SplitContainer)).EndInit();
+			this.chat_SplitContainer.ResumeLayout(false);
+			this.messages_SplitContainer.Panel1.ResumeLayout(false);
+			this.messages_SplitContainer.Panel2.ResumeLayout(false);
+			this.messages_SplitContainer.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.messages_SplitContainer)).EndInit();
+			this.messages_SplitContainer.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.SplitContainer mainSplitContainer;
-		private System.Windows.Forms.SplitContainer controlSplitContainer;
-		private System.Windows.Forms.FlowLayoutPanel listOfChatFlowLayoutPanel;
-		private System.Windows.Forms.SplitContainer chatSplitContainer;
+		private System.Windows.Forms.SplitContainer main_SplitContainer;
+		private System.Windows.Forms.SplitContainer control_SplitContainer;
+		private System.Windows.Forms.FlowLayoutPanel listOfChat_FlowLayoutPanel;
+		private System.Windows.Forms.SplitContainer chat_SplitContainer;
+		private System.Windows.Forms.SplitContainer messages_SplitContainer;
+		private System.Windows.Forms.TextBox message_TextBox;
+		private System.Windows.Forms.FlowLayoutPanel chat_FlowLayoutPanel;
 	}
 }
 
--- Messenger/Messenger/Messenger.cs
+++ Messenger/Messenger/Messenger.cs
@@ -36,7 +36,7 @@ namespace Messenger
 				{
 					AutoSize = true,
 					Location = new System.Drawing.Point(60, 5),
-					Name = "label3",
+					Name = "Something",
 					Size = new System.Drawing.Size(57, 13),
 					TabIndex = 1,
 					Text = "Something " + i.ToString()
@@ -47,7 +47,7 @@ namespace Messenger
 					Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right))),
 					AutoSize = true,
 					Location = new System.Drawing.Point(227, 5),
-					Name = "label4",
+					Name = "Time",
 					Size = new System.Drawing.Size(30, 13),
 					TabIndex = 2,
 					Text = "Time",
@@ -59,39 +59,159 @@ namespace Messenger
 					AutoSize = true,
 					ForeColor = System.Drawing.Color.DarkRed,
 					Location = new System.Drawing.Point(65, 33),
-					Name = "label5",
+					Name = "Message",
 					Size = new System.Drawing.Size(50, 13),
 					TabIndex = 3,
 					Text = "Message"
 				};
 
+				Label label_4 = new System.Windows.Forms.Label
+				{
+					AutoSize = true,
+					Location = new System.Drawing.Point(0, 0),
+					Name = "ID",
+					Size = new System.Drawing.Size(50, 13),
+					TabIndex = 3,
+					Text = "ID" + i.ToString(),
+					Visible = false
+				};
+				
 				Panel panel = new Panel
 				{
 					BackColor = System.Drawing.Color.Gray,
 					Location = new System.Drawing.Point(3, 3),
 					Name = "panel1",
-					Size = new System.Drawing.Size(listOfChatFlowLayoutPanel.ClientSize.Width - 7, 58),
+					Size = new System.Drawing.Size(listOfChat_FlowLayoutPanel.ClientSize.Width - 7, 58),
 					TabIndex = 0
 				};
+				panel.Controls.Add(label_4);
 				panel.Controls.Add(label_1);
 				panel.Controls.Add(label_2);
 				panel.Controls.Add(label_3);
 				panel.Controls.Add(pictureBox_1);
 
+				panel.Click += new EventHandler(this.CreateChat);
 
-				listOfChatFlowLayoutPanel.Controls.Add(panel);
+				this.listOfChat_FlowLayoutPanel.Controls.Add(panel);
+			}
+			
+			this.message_TextBox.Select();
+		}
+
+		private string chatID = "";
+
+		private void CreateChat(object sender, EventArgs e)
+		{
+			if (((Panel)sender).Controls[0].Text != chatID)
+			{
+				chatID = ((Panel)sender).Controls[0].Text;
+
+				this.chat_FlowLayoutPanel.Controls.Clear();
+
+				for (int i = 0; i < 7; ++i)
+				{
+					PictureBox pictureBox_1 = new System.Windows.Forms.PictureBox
+					{
+						BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64))))),
+						Location = new System.Drawing.Point(3, 3),
+						Name = "pictureBox1",
+						Size = new System.Drawing.Size(52, 52),
+						TabIndex = 0,
+						TabStop = false
+					};
+
+					Label label_1 = new System.Windows.Forms.Label
+					{
+						AutoSize = true,
+						Location = new System.Drawing.Point(60, 5),
+						Name = "Something",
+						Size = new System.Drawing.Size(57, 13),
+						TabIndex = 1,
+						Text = "Something " + i.ToString()
+					};
+
+					Label label_2 = new System.Windows.Forms.Label
+					{
+						Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right))),
+						AutoSize = true,
+						Location = new System.Drawing.Point(227, 5),
+						Name = "Time",
+						Size = new System.Drawing.Size(30, 13),
+						TabIndex = 2,
+						Text = "Time",
+						TextAlign = System.Drawing.ContentAlignment.TopRight
+					};
+
+					Label label_3 = new System.Windows.Forms.Label
+					{
+						AutoSize = true,
+						ForeColor = System.Drawing.Color.DarkRed,
+						Location = new System.Drawing.Point(65, 33),
+						Name = "Message",
+						Size = new System.Drawing.Size(50, 13),
+						TabIndex = 3,
+						Text = "Message"
+					};
+
+					Label label_4 = new System.Windows.Forms.Label
+					{
+						AutoSize = true,
+						Location = new System.Drawing.Point(0, 0),
+						Name = "ID",
+						Size = new System.Drawing.Size(50, 13),
+						TabIndex = 3,
+						Text = "ID" + i.ToString(),
+						Visible = false
+					};
+
+					Panel panel = new Panel
+					{
+						BackColor = System.Drawing.Color.Gray,
+						Location = new System.Drawing.Point(3, 3),
+						Name = "panel1",
+						Size = new System.Drawing.Size(listOfChat_FlowLayoutPanel.ClientSize.Width - 7, 58),
+						TabIndex = 0
+					};
+					panel.Controls.Add(label_1);
+					panel.Controls.Add(label_2);
+					panel.Controls.Add(label_3);
+					panel.Controls.Add(pictureBox_1);
+
+
+					this.chat_FlowLayoutPanel.Controls.Add(panel);
+				}
+
 			}
 		}
 
-		private void ListOfChatFlowLayoutPanel_SizeChanged(object sender, EventArgs e)
+		private void ListOfChat_FlowLayoutPanel_SizeChanged(object sender, EventArgs e)
 		{
-			foreach (Control control in listOfChatFlowLayoutPanel.Controls)
+			foreach (Control control in listOfChat_FlowLayoutPanel.Controls)
 			{
-				control.Width = listOfChatFlowLayoutPanel.ClientRectangle.Width - 7;
+				control.Width = listOfChat_FlowLayoutPanel.ClientRectangle.Width - 7;
 			}
-			listOfChatFlowLayoutPanel.SuspendLayout();
-			listOfChatFlowLayoutPanel.ResumeLayout();
-			listOfChatFlowLayoutPanel.PerformLayout();
+			listOfChat_FlowLayoutPanel.SuspendLayout();
+			listOfChat_FlowLayoutPanel.ResumeLayout();
+			listOfChat_FlowLayoutPanel.PerformLayout();
+		}
+
+		
+
+		private void Chat_FlowLayoutPanel_SizeChanged(object sender, EventArgs e)
+		{
+			foreach (Control control in listOfChat_FlowLayoutPanel.Controls)
+			{
+				control.Width = listOfChat_FlowLayoutPanel.ClientRectangle.Width - 7;
+			}
+			listOfChat_FlowLayoutPanel.SuspendLayout();
+			listOfChat_FlowLayoutPanel.ResumeLayout();
+			listOfChat_FlowLayoutPanel.PerformLayout();
+		}
+
+
+		private void Message_TextBox_TextChanged(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
