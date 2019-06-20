dc77dd75b351c7117a20695f0e533eb0c6f100e2
Артемий Мысечко <mysechko01@gmail.com>
2019-06-21 01:26:29 +0300

    RSA algorithm

---
+++ Data encryption/Data encryption/DataEncryption.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace Data_encryption
+{
+	class DataEncryption
+	{
+		public static Tuple<string, string> generatePrivateKey()
+		{
+			var RSA = new RSACryptoServiceProvider();
+			string pr_key = RSA.ToXmlString(true);
+			string pb_key = RSA.ToXmlString(false);
+			return new Tuple<string, string> (pr_key, pb_key);
+		}
+
+		public static string Decrypt(string data, string key)
+		{
+			var RSA = new RSACryptoServiceProvider();
+			var dataArray = data.Split(new char[] { ',' });
+			byte[] dataByte = new byte[dataArray.Length];
+
+			for (int i = 0; i < dataArray.Length; i++)
+			{
+				dataByte[i] = Convert.ToByte(dataArray[i]);
+			}
+
+			RSA.FromXmlString(key);
+			var decryptedByte = RSA.Decrypt(dataByte, false);
+			UnicodeEncoding encoding = new UnicodeEncoding();
+			return encoding.GetString(decryptedByte);
+		}
+
+		public static string Encrypt(string data, string key)
+		{
+			var RSA = new RSACryptoServiceProvider();
+			RSA.FromXmlString(key);
+			UnicodeEncoding encoding = new UnicodeEncoding();
+			var dataToEncrypt = encoding.GetBytes(data);
+			var encryptedByteArray = RSA.Encrypt(dataToEncrypt, false).ToArray();
+			var length = encryptedByteArray.Count();
+			var item = 0;
+			var sb = new StringBuilder();
+
+			foreach (var x in encryptedByteArray)
+			{
+				item++;
+				sb.Append(x);
+
+				if (item < length)
+					sb.Append(",");
+			}
+
+			return sb.ToString();
+		}
+	}
+}
--- Data encryption/Data encryption/Program.cs
+++ Data encryption/Data encryption/Program.cs
@@ -10,6 +10,27 @@ namespace Data_encryption
 	{
 		static void Main(string[] args)
 		{
+			string erere = "";
+			int i = 0;
+			while (i < 30)
+			{
+				erere += i.ToString();
+				++i;
+			}
+			Console.WriteLine(erere.Length);
+			Console.WriteLine('\n');
+			Tuple<string, string> keys = DataEncryption.generatePrivateKey();
+
+			Console.WriteLine(keys.Item1);
+			Console.WriteLine(keys.Item2);
+			Console.WriteLine(erere);
+			string x = DataEncryption.Encrypt(erere, keys.Item2);
+			Console.WriteLine(x);
+			string y = DataEncryption.Decrypt(x, keys.Item1);
+			Console.WriteLine(y);
+
+
+			Console.ReadKey();
 		}
 	}
 }
